a6c91afd99e1624cbd760d67eb2c90cb250d11d2
clarajeon <clarajeon126@gmail.com>
2021-03-27 14:05:35 -0400

    scroll view edits

--- beans/View Controllers/BeanPickerSingleViewController.swift
+++ beans/View Controllers/BeanPickerSingleViewController.swift
@@ -13,17 +13,23 @@ class BeanPickerSingleViewController: UIViewController {
     
     @IBOutlet var backgroundView: UIView!
     
+    @IBOutlet var circleViewArray: [UIView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         //make it a circular button
         buttonHome.layer.cornerRadius = buttonHome.frame.width / 2
         
+        for x in 0..<circleViewArray.count {
+            let circleInQuestion = circleViewArray[x]
+            
+            circleInQuestion.layer.cornerRadius = 15
+        }
         
         
         // Do any additional setup after loading the view.
     }
-    
 
     /*
     // MARK: - Navigation
