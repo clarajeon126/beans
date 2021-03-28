0db196e8ab8a74edd78d5ab3f0c13d10c5931072
clarajeon <clarajeon126@gmail.com>
2021-03-28 04:53:45 -0400

    ml model implemented and works kindaaa

--- beans/View Controllers/Bean AR VC/HomeBeanViewController.swift
+++ beans/View Controllers/Bean AR VC/HomeBeanViewController.swift
@@ -6,16 +6,162 @@
 //
 
 import UIKit
+import SceneKit
+import ARKit
+import Vision
 
-class HomeBeanViewController: UIViewController {
+class HomeBeanViewController: UIViewController, ARSCNViewDelegate {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    var visionRequests = [VNRequest]()
+    let dispatchQueueML = DispatchQueue(label: "com.hw.dispatchqueueml") // A Serial Queue
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //set up ar scene
+        sceneView.delegate = self
+        sceneView.showsStatistics = false
+        
+        let scene = SCNScene()
+        
+        sceneView.scene = scene
+        
+        //image detection model yolo
+        guard let selectedModel = try? VNCoreMLModel(for: YOLOv3Tiny().model) else {
+            fatalError("error model would not load")
+        }
+        
+        print("ouside")
+        //set up request
+        let classificationRequest = VNCoreMLRequest(model: selectedModel) { (request, error) in
+            DispatchQueue.main.async(execute: {
+                if let results = request.results {
+                    //print(results)
+                    self.useDataForDisplays(results)
+                    //print("inside")
+                }
+            })
+        }
+        self.visionRequests = [classificationRequest]
+        
+        loopCoreMLUpdate()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        // Enable plane detection
+        configuration.planeDetection = .horizontal
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    func loopCoreMLUpdate() {
+        // Continuously run CoreML whenever it's ready. (Preventing 'hiccups' in Frame Rate)
+        
+        dispatchQueueML.async {
+            // 1. Run Update.
+            self.updateCoreML()
+            
+            // 2. Loop this function.
+            self.loopCoreMLUpdate()
+        }
+        
+    }
 
-        // Do any additional setup after loading the view.
+    func useDataForDisplays(_ results: [Any]) {
+        for observation in results where observation is VNRecognizedObjectObservation {
+            guard let objectObservation = observation as? VNRecognizedObjectObservation else {
+                continue
+            }
+            print(objectObservation.labels.count)
+            //let topLableObservation = objectObservation.labels[0]
+            //for x in 0..<4{
+                let observation = objectObservation.labels[0]
+                let objectBounds = VNImageRectForNormalizedRect(objectObservation.boundingBox, Int(sceneView.frame.width), Int(sceneView.frame.height))
+                
+                let shapeLayer = self.createRoundedRectLayerWithBounds(objectBounds)
+                
+                let textLayer = self.createTextSubLayerInBounds(objectBounds, identifier: observation.identifier, confidence: observation.confidence)
+                
+                print(observation.identifier)
+            //}
+            
+            //print(topLableObservation.identifier)
+        }
+        /*for x in 0..<results.count{
+            print(results[x])
+        }*/
+    }
+    
+    
+    func updateCoreML() {
+        ///////////////////////////
+        // Get Camera Image as RGB
+        let pixbuff : CVPixelBuffer? = (sceneView.session.currentFrame?.capturedImage)
+        if pixbuff == nil { return }
+        let ciImage = CIImage(cvPixelBuffer: pixbuff!)
+        // Note: Not entirely sure if the ciImage is being interpreted as RGB, but for now it works with the Inception model.
+        // Note2: Also uncertain if the pixelBuffer should be rotated before handing off to Vision (VNImageRequestHandler) - regardless, for now, it still works well with the Inception model.
+        
+        ///////////////////////////
+        // Prepare CoreML/Vision Request
+        let imageRequestHandler = VNImageRequestHandler(ciImage: ciImage, options: [:])
+        // let imageRequestHandler = VNImageRequestHandler(cgImage: cgImage!, orientation: myOrientation, options: [:]) // Alternatively; we can convert the above to an RGB CGImage and use that. Also UIInterfaceOrientation can inform orientation values.
+        
+        ///////////////////////////
+        // Run Image Request
+        do {
+            try imageRequestHandler.perform(self.visionRequests)
+        } catch {
+            print(error)
+        }
         
     }
     
+    func createTextSubLayerInBounds(_ bounds: CGRect, identifier: String, confidence: VNConfidence) -> CATextLayer {
+        let textLayer = CATextLayer()
+        textLayer.name = "Object Label"
+        let formattedString = NSMutableAttributedString(string: String(format: "\(identifier)\nConfidence:  %.2f", confidence))
+        let largeFont = UIFont(name: "Helvetica", size: 24.0)!
+        formattedString.addAttributes([NSAttributedString.Key.font: largeFont], range: NSRange(location: 0, length: identifier.count))
+        textLayer.string = formattedString
+        textLayer.bounds = CGRect(x: 0, y: 0, width: bounds.size.height - 10, height: bounds.size.width - 10)
+        textLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        textLayer.shadowOpacity = 0.7
+        textLayer.shadowOffset = CGSize(width: 2, height: 2)
+        textLayer.foregroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [0.0, 0.0, 0.0, 1.0])
+        textLayer.contentsScale = 2.0 // retina rendering
+        // rotate the layer into screen orientation and scale and mirror
+        textLayer.setAffineTransform(CGAffineTransform(rotationAngle: CGFloat(.pi / 2.0)).scaledBy(x: 1.0, y: -1.0))
+        return textLayer
+    }
+    
+    func createRoundedRectLayerWithBounds(_ bounds: CGRect) -> CALayer {
+        let shapeLayer = CALayer()
+        shapeLayer.bounds = bounds
+        shapeLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        shapeLayer.name = "Found Object"
+        shapeLayer.backgroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [1.0, 1.0, 0.2, 0.4])
+        shapeLayer.cornerRadius = 7
+        return shapeLayer
+    }
+}
 
     /*
     // MARK: - Navigation
@@ -26,5 +172,3 @@ class HomeBeanViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
-}
