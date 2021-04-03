9b2af2032541bc656735a9c08b07b9b97c5b78e9
Hellie Chen <helliechen@Hellies-Mac.local>
2021-04-03 13:29:44 -0700

    committing

--- beans/View Controllers/Bean AR VC/ChemBeanViewController.swift
+++ beans/View Controllers/Bean AR VC/ChemBeanViewController.swift
@@ -229,9 +229,9 @@ class ChemBeanViewController: UIViewController, ARSCNViewDelegate {
     
                                 }
                                 //comment out if this goes wrong
-                                /*for x in 0..<chemBeanArray.count{
+                                for x in 0..<chemBeanArray.count{
                                     displayInfo.append(chemBeanArray[x].info)
-                                }*/
+                                }
                                 
                                 print(displayInfo)
                                 
